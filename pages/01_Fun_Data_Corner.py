35f00a2e0e3195ad421957b268ee7c94b6cfa7e4
vikram-dev1125 <vikramjustmus1k@gmail.com>
2025-05-16 17:57:56 -0400

    Update 01_Fun_Data_Corner.py

--- pages/01_Fun_Data_Corner.py
+++ pages/01_Fun_Data_Corner.py
@@ -85,7 +85,7 @@ def main():
     st.header("Within‑Race SAT Distribution (1100–1600)")
 
     st.markdown("""
-    Hello fellow data nerd! Here you can find numerous different angles of data visualization from the dataset I am using, updated as my dataset improves.
+    Hello fellow data nerds! Here you can find numerous different angles of data visualization from the dataset I am using, updated as my dataset improves.
 
     I do want to note that since the data were taken from a subreddit dedicated to college results, there is a volunteer response bias in play that definitely overestimates all metrics for the typical student. Nevertheless, there aren't any better sources for this data that I could find, so we will have to roll with it!
     """)
